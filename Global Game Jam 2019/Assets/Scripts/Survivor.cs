5b552d7d4fad6c00f2e1851522209b1976c8b4ea
DESKTOP-SC6I7KB\prest <preston.grau@gmail.com>
2019-01-26 15:29:33 -0600

    first commit

---
+++ Global Game Jam 2019/Assets/Scripts/Survivor.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Survivor : MonoBehaviour
+{
+    /// <summary>
+    /// This will control aspects of the player that is not movement
+    /// </summary>
+
+    public float maxHealth = 100f;
+    public float curHealth;
+
+    private bool hasGasMaskBottom = false;
+    private bool hasGasMaskTop = false;
+    private bool fullgasMask = false;
+    public bool inHome = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(hasGasMaskBottom && hasGasMaskTop && inHome)
+        {
+            fullgasMask = true;
+        }
+
+        if (inHome == false && fullgasMask == false)
+        {
+            curHealth -= 1f;
+        }
+        else if(inHome == false && fullgasMask == true)
+        {
+            curHealth -= .5f;
+        }
+        else if (inHome == true && curHealth < maxHealth)
+        {
+            curHealth += 1f;
+        }
+    }
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        if(coll.name == "pixel house")
+        {
+            inHome = true;
+        }
+
+        PickUp pup = coll.GetComponent<PickUp>();
+
+        if(pup == null)
+        {
+            return;
+        }
+
+        if(pup.itemType == PickUp.eType.gasMaskBottom)
+        {
+            hasGasMaskBottom = true;
+            Destroy(coll.gameObject);
+        }
+        if(pup.itemType == PickUp.eType.gasMaskTop)
+        {
+            hasGasMaskTop = true;
+            Destroy(coll.gameObject);
+        }
+        
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "pixel house")
+        {
+            inHome = false;
+        }
+    }
+}
---
+++ Global Game Jam 2019/Assets/Scripts/TopDownMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownMovement : MonoBehaviour
+{
+    public GameObject player;
+    public float moveSpeed = 3f;
+
+    private Rigidbody2D rigid;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = player.GetComponent<Rigidbody2D>();
+        anim = player.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        player.transform.position += movement * moveSpeed * Time.deltaTime;
+
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            anim.speed = 1;
+            anim.Play("Walk_1");
+        }
+        else if(Input.GetAxis("Horizontal") < 0)
+        {
+            anim.speed = 1;
+            anim.Play("Walk_2");
+        }
+        else if(Input.GetAxis("Vertical") > 0)
+        {
+            anim.speed = 1;
+            anim.Play("Walk_3");
+        }
+        else if(Input.GetAxis("Vertical") < 0)
+        {
+            anim.speed = 1;
+            anim.Play("Walk_0");
+        }
+        else
+        {
+            anim.speed = 0;
+        }
+    }
+}
